8c3c1cbc2cce09fad6a1e6def8be7559f0bbf211
Pedro dos Santos <67605385+97pedros@users.noreply.github.com>
2022-11-18 19:28:51 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Renomeador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_remover = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirPastaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fecharToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRemover = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.chk_mtn_numeros = new System.Windows.Forms.CheckBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_remover
+            // 
+            this.btn_remover.Location = new System.Drawing.Point(350, 56);
+            this.btn_remover.Name = "btn_remover";
+            this.btn_remover.Size = new System.Drawing.Size(75, 23);
+            this.btn_remover.TabIndex = 0;
+            this.btn_remover.Text = "Renomear";
+            this.btn_remover.UseVisualStyleBackColor = true;
+            this.btn_remover.Click += new System.EventHandler(this.btn_remover_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuStrip1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip1.Size = new System.Drawing.Size(438, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirPastaToolStripMenuItem,
+            this.fecharToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // abrirPastaToolStripMenuItem
+            // 
+            this.abrirPastaToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.abrirPastaToolStripMenuItem.Name = "abrirPastaToolStripMenuItem";
+            this.abrirPastaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.abrirPastaToolStripMenuItem.Text = "&Abrir Pasta";
+            this.abrirPastaToolStripMenuItem.Click += new System.EventHandler(this.abrirPastaToolStripMenuItem_Click);
+            // 
+            // fecharToolStripMenuItem
+            // 
+            this.fecharToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.fecharToolStripMenuItem.Name = "fecharToolStripMenuItem";
+            this.fecharToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.fecharToolStripMenuItem.Text = "&Fechar";
+            this.fecharToolStripMenuItem.Click += new System.EventHandler(this.fecharToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.sobreToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreToolStripMenuItem1});
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // sobreToolStripMenuItem1
+            // 
+            this.sobreToolStripMenuItem1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.sobreToolStripMenuItem1.Name = "sobreToolStripMenuItem1";
+            this.sobreToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.sobreToolStripMenuItem1.Text = "Sobre";
+            this.sobreToolStripMenuItem1.Click += new System.EventHandler(this.sobreToolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Texto para remover";
+            // 
+            // txtRemover
+            // 
+            this.txtRemover.Location = new System.Drawing.Point(12, 56);
+            this.txtRemover.Name = "txtRemover";
+            this.txtRemover.Size = new System.Drawing.Size(332, 20);
+            this.txtRemover.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 113);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(438, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.Stretch = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(43, 17);
+            this.toolStripStatusLabel1.Text = "Pronto";
+            // 
+            // chk_mtn_numeros
+            // 
+            this.chk_mtn_numeros.AutoSize = true;
+            this.chk_mtn_numeros.Location = new System.Drawing.Point(12, 82);
+            this.chk_mtn_numeros.Name = "chk_mtn_numeros";
+            this.chk_mtn_numeros.Size = new System.Drawing.Size(182, 17);
+            this.chk_mtn_numeros.TabIndex = 8;
+            this.chk_mtn_numeros.Text = "Manter apenas Numeros do Final";
+            this.chk_mtn_numeros.UseVisualStyleBackColor = true;
+            this.chk_mtn_numeros.CheckedChanged += new System.EventHandler(this.chk_mtn_numeros_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 135);
+            this.Controls.Add(this.chk_mtn_numeros);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtRemover);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_remover);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "97Pedros - Renomeador";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_remover;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fecharToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRemover;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripMenuItem abrirPastaToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.CheckBox chk_mtn_numeros;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Renomeador
+{
+    public partial class Form1 : Form
+    {
+        string pasta;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fecharToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Janela de informações
+        private void sobreToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutBox1 sobre = new AboutBox1();
+            sobre.Show();
+        }
+        //Função para a janela de seleção de pastas
+        void abrir()
+        {
+            FolderBrowserDialog navPasta = new FolderBrowserDialog();
+            if (navPasta.ShowDialog() == DialogResult.OK)
+            {
+                pasta = navPasta.SelectedPath;
+                toolStripProgressBar1.Value = 0;
+            }
+        }
+
+        //Função para remover palavras do nome do arquivo
+        void renomear()
+        {
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(@pasta);
+                FileInfo[] infos = d.GetFiles();
+                int max = 0;
+                foreach (FileInfo f in infos)
+                {
+                    max++;
+                    toolStripProgressBar1.Maximum = max;
+                }
+                foreach (FileInfo f in infos)
+                {
+                    toolStripStatusLabel1.Text = "trabalhando em" + f.Name;
+                    File.Move(f.FullName, f.FullName.Replace(txtRemover.Text, ""));
+                    toolStripProgressBar1.Increment(1);
+
+                }
+                toolStripStatusLabel1.Text = "Pronto";
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Selecione uma pasta!", "Erro");
+                abrir();
+            }
+        }
+
+        //Função para manter apenas os números do nome do arquivo
+        void manter_numeros()
+        {
+            string num;
+            DirectoryInfo d = new DirectoryInfo(@pasta);
+            FileInfo[] infos = d.GetFiles();
+            int max = 0;
+            foreach (FileInfo f in infos)
+            {
+                max++;
+                toolStripProgressBar1.Maximum = max;
+            }
+            foreach (FileInfo f in infos)
+            {
+                toolStripStatusLabel1.Text = "trabalhando em" + f.Name;
+                num = f.FullName.Substring(f.FullName.Length - 7);
+                File.Move(f.FullName, f.FullName.Replace(f.Name, num));
+                toolStripProgressBar1.Increment(1);
+
+            }
+            toolStripStatusLabel1.Text = "Pronto";
+        }
+
+        //menu abrir pasta
+        private void abrirPastaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            abrir();
+            this.Text = this.Text + " Trabalhando em: " + pasta;
+        }
+
+        //botão renomear
+        private void btn_remover_Click(object sender, EventArgs e)
+        {
+            if(chk_mtn_numeros.Checked)
+            {
+                manter_numeros();
+            }
+            else
+            {
+                renomear();
+            }
+        }
+        
+        //checkbox de manter os números
+        private void chk_mtn_numeros_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chk_mtn_numeros.Checked)
+            {
+                txtRemover.Enabled = false;
+            }
+            else
+            {
+                txtRemover.Enabled = true;
+            }
+        }
+    }
+}
